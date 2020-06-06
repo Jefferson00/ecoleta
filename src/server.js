6661c08eae03b58afbc189f5bf38c02353e5d983
Jefferson00 <jeffersonts00@gmail.com>
2020-06-06 15:09:04 -0300

    Backend adcionado

--- src/server.js
+++ src/server.js
@@ -1,11 +1,96 @@
 const express = require('express')
-
 const server = express()
+
+//pegar o banco de dados
+const db = require('./database/db')
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({extended: true}))
+
+//utilizar temlate engine
+const nunjucks = require("nunjucks")
+//configuração do nunjucks, informando inde estão as páginas
+// html, servidor e que não utilizará cache
+nunjucks.configure("src/views", {
+    express: server, 
+    noCache: true
+})
 
 //configurar rotas
 
 server.get("/", (req, res) =>{
+    return res.render("index.html")
+}) 
 
+server.get("/create-point", (req, res) =>{
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) =>{
+//inserir dados no banco de dados
+
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+       req.body.image,
+       req.body.name,
+       req.body.address,
+       req.body.address2,
+       req.body.state,
+       req.body.city,
+       req.body.items,
+    ]
+
+    function afterInsertData(err){
+        if(err){
+            console.log(err)
+            return res.render("create-point.html", {saved: false})
+        }
+
+        console.log("Cadastro com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
+
+server.get("/search", (req, res) =>{
+
+    const search = req.query.search
+
+    if (search == ""){
+        return res.render("search.html", {total:0})
+    }
+
+    //pegar dados do banco de dados
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+
+        if(err){
+            return console.log(err)
+        }
+        const total = rows.length
+
+        return res.render("search.html", {places: rows, total})
+    })
+    
 })
 
 
